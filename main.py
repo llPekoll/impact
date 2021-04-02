278415b25570e52e0b9e55a40cf4b8a117807e1d
peko <yoyo.mepa@gmail.com>
2021-04-02 11:48:55 +0200

    generate mock datas

---
+++ main.py
@@ -0,0 +1,29 @@
+import random
+from datetime import datetime, timedelta
+import uuid
+
+
+mock_data = [{"id": "xxxx-xxxx-xxxx-xxxx", 
+                "event": "impressions", 
+                "timestamp": 00000000000}
+                ]
+
+mock_for_test = [] 
+event_type = ["impressions", "video_start", "video_25", "video_50", "video_75", "video_completed"] 
+random_hours = [3 , 4 , 5, 6, 8]
+
+for x in range(6):
+    # random hours + GMT+1
+    timestamp = datetime.now() + timedelta(hours=1) + timedelta(hours=random.choice(random_hours))
+    dict_mock = {
+        "id": str(uuid.uuid4())[4:23],
+        "event": random.choice(event_type),
+        "timestamp": int(timestamp.timestamp())
+    }
+    mock_for_test.append(dict_mock)
+print(mock_for_test)
+
+# def first_func():
+#     event = [{"nb_event":3,
+#     "event_type":3,
+#     "date":3,}]
